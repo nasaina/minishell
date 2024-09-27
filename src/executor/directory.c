c6f297b034c1b178a6db271010cbdba2e62ec031
Sin.k <miraelsink@gmail.com>
2024-09-27 15:30:51 +0300

    find directory ok

--- src/executor/directory.c
+++ src/executor/directory.c
@@ -6,7 +6,7 @@
 /*   By: maandria <maandria@student.42antanana      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 13:55:42 by maandria          #+#    #+#             */
-/*   Updated: 2024/09/26 14:15:04 by maandria         ###   ########.fr       */
+/*   Updated: 2024/09/27 15:29:28 by maandria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ms_pwd(void)
 	char	*pwd;
 
 	pwd = getcwd(NULL,0);
-	printf("%s", pwd);
+	printf("%s\n", pwd);
 }
 
 void	ms_cd(char *str)
