c4a988696dd05d24b6b7ee9585c8bf449e4ad994
Sin.k <miraelsink@gmail.com>
2024-12-21 14:46:32 +0300

    exec leaks erased (maybe)

--- src/executor/path_access.c
+++ src/executor/path_access.c
@@ -6,7 +6,7 @@
 /*   By: maandria <maandria@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 13:16:46 by maandria          #+#    #+#             */
-/*   Updated: 2024/12/20 13:35:57 by maandria         ###   ########.fr       */
+/*   Updated: 2024/12/21 14:40:41 by maandria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,30 +36,29 @@ char	*check_path(char **pathlist, t_ast *ast)
 	int		i;
 	char	*path;
 	char	*command;
-	char	*tmp;
 	char	*path_tmp;
 
 	i = 0;
 	path = NULL;
 	command = ft_strjoin("/", ast->cmd->args[0]);
+	if (!pathlist)
+	{
+		free(command);
+		return (NULL);
+	}
 	while (pathlist[i])
 	{
 		path_tmp = ft_strjoin(pathlist[i], command);
 		if (access(path_tmp, F_OK) == 0)
 		{
-			tmp = ft_strjoin(pathlist[i], "/");
-			path  = ft_strjoin(tmp, ast->cmd->args[0]);
-			free(tmp);
+			path = ft_strdup(path_tmp);
 			free(path_tmp);
 			free(command);
 			free_tab(pathlist);
 			return (path);
 		}
-		else
-		{
-			free(path_tmp);
-			i++;
-		}
+		free(path_tmp);
+		i++;
 	}
 	free_tab(pathlist);
 	free(command);
